f04b692c31b9d6b461c6387aa10648ebfe80e9da
wgd <wanggd2022@shanghaitech.edu.cn>
2024-10-25 17:18:32 +0800

    [debug] try to change temporary register used by JL emitting

--- src/dynarec/rv64/dynarec_rv64_0f.c
+++ src/dynarec/rv64/dynarec_rv64_0f.c
@@ -1786,7 +1786,7 @@ uintptr_t dynarec64_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
                     //GO(NO,YES)
                     //NEZ=1  EQZ=0
                     //op1<op2 then jmp -> x1==1 -> YES -> GO(EQZ,NEZ)
-                GO(SLT(x3, dyn->insts[ninst].op1, dyn->insts[ninst].op2);
+                GO(SLT(x1, dyn->insts[ninst].op1, dyn->insts[ninst].op2);
                     NOP();
                     NOP(), EQZ, NEZ, X_SF | X_OF)
             }
